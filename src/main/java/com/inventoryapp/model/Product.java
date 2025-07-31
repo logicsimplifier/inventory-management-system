9932e8faf36757a35f0c922821d12a16cef12ede
devindawanasinghe <devindawanasinghe@gmail.com>
2025-07-31 07:29:30 +0530

    setup-login-ui&backend

--- src/main/java/com/inventoryapp/model/Product.java
+++
@@ -1,4 +0,0 @@
-package com.inventoryapp.model;
-
-public class Product {
-}
---
+++ src/test/java/com/inventoryapp/TestDB.java
@@ -0,0 +1,11 @@
+// package com.inventoryapp;
+
+// import com.inventoryapp.util.DBUtils;
+
+// public class TestDB {
+
+//     public static void main(String[] args) {
+//         DBUtils.getConnection();
+//     }
+
+// }
