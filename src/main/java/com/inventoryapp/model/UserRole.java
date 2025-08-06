e982183bb3d96ed84ed639a1e77e01a6761843fb
devindawanasinghe <devindawanasinghe@gmail.com>
2025-08-06 18:51:03 +0530

    user-role-inventory.db file confilt fix

--- src/main/java/com/inventoryapp/model/UserRole.java
+++ src/main/java/com/inventoryapp/model/UserRole.java
@@ -1,5 +1,40 @@
 package com.inventoryapp.model;
 
 public class UserRole {
-    
+
+    //Hiding internal data fron other classes(Encapsulation)
+    private String username;
+    private String password;
+    private String role;
+
+    public UserRole(String username, String password, String role){
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+    //getter, setter method
+    public String getUsername(){
+        return username;
+    }
+
+    public void setUsername(String username){
+        this.username = username;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+
+    public void setPassword(String password){
+        this.password = password;
+    }
+
+    public String getRole(){
+        return role;
+    }
+
+    public void setRole(String role){
+        this.role = role;
+    }
 }
